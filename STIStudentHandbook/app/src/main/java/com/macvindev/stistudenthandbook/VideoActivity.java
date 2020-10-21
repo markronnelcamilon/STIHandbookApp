8f7d7affc7cdb24aae57e381a86b8a4b567bc547
markcamilon <markcamilon@gmail.com>
2020-10-21 14:53:23 +0800

    QRActivity added

--- STIStudentHandbook/app/src/main/java/com/macvindev/stistudenthandbook/VideoActivity.java
+++ STIStudentHandbook/app/src/main/java/com/macvindev/stistudenthandbook/VideoActivity.java
@@ -3,6 +3,7 @@ package com.macvindev.stistudenthandbook;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class VideoActivity extends AppCompatActivity {
 
@@ -10,5 +11,6 @@ public class VideoActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_video);
+        Toast.makeText(this, "Video", Toast.LENGTH_SHORT).show();
     }
 }
